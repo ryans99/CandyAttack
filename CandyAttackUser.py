9bd652078d4ced2239d35cbf23ef35bb3ccd91b0
Justin Vicente <791709@seq.org>
2017-05-14 14:17:40 -0700

    Add files via upload

---
+++ CandyAttackUser.py
@@ -0,0 +1,12 @@
+class User():
+    brocBucks = 0
+    levelOfUser = 0
+    levelsCompleted = 0
+    candyCoin = 0
+    def __init__(self,brocBucks, levelOfUser, levelsCompleted, candyCoin):
+        self.bb = brocBucks
+        self.lou = levelOfUser
+        self.lc = levelsCompleted
+        self.cc = candyCoin
+    dict = {'brocBucks': 0, 'levelOfUser': 0, 'levelsCompleted': 0, 'candyCoins': 0}
+    dict['brocBucks'] = 0
