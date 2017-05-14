37b320caf9dfa8349b5242ea7c2e69ebfa0ab0a4
790985 <790985@seq.org>
2017-05-14 00:13:30 -0700

    Update Tower.py

--- Tower.py
+++ Tower.py
@@ -2,14 +2,23 @@
 #Justin Vicinte, Christopher Campos
 # 13 may, 2017
 class Tower():
-
+    MAX_LEVEL = 3 #Constant
     #Initiation for Towers
     def __init__(self,TowerType,TowerCost,dps):
         self.type = TowerType
         self.cost = TowerCost
         self.damadge = dps
         self.location = None
+        self.level = 1
 
     def shoot(self,Enemy):
         Enemy.health -= self.damadge
         return Enemy.health
+        #Add Code For Animation
+    
+    def upgrade(self):
+        if (self.level + 1) > MAX_LEVEL:
+            return null
+        else:
+            self.level += 1
+            self.damadge += 0.15*(self.damadge) # Increases in percent before multiplication
