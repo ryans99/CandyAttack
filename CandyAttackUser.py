cad17bf3f5921949d965ebea8e150d88860e763c
790985 <790985@seq.org>
2017-05-15 08:48:20 -0700

    Update CandyAttackUser.py

--- CandyAttackUser.py
+++ CandyAttackUser.py
@@ -1,12 +1,11 @@
 class User():
     brocBucks = 0
-    levelOfUser = 0
+    currentLevel = 0
     levelsCompleted = 0
     candyCoin = 0
     def __init__(self,brocBucks, levelOfUser, levelsCompleted, candyCoin):
-        self.bb = brocBucks
-        self.lou = levelOfUser
-        self.lc = levelsCompleted
-        self.cc = candyCoin
+        self.bucks = brocBucks
+        self.current = CurrentLevel
+        self.completed = levelsCompleted
+        self.coin = candyCoin
     dict = {'brocBucks': 0, 'levelOfUser': 0, 'levelsCompleted': 0, 'candyCoins': 0}
-    dict['brocBucks'] = 0
